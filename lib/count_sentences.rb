62a2986a30be9a33c04a388643d80d0603946ea6
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-08-08 17:47:57 -0500

    commit tree e2efb50f311febf223f81062dea359c3fc722552

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if (self[-1] == ".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if (self[-1] == "?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if (self[-1] == "!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    
+    self.scan(/[.?!]/).length
+    # self.split(" ").length
 
+    # print self.scan(/\w+.?!/)
+  
+    
   end
 end
